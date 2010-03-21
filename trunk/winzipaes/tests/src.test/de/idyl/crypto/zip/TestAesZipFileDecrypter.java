84afb503ae939f95bcd878d481909fbe3f2703c3
olaf.merkert <olaf.merkert@d6a6f5eb-4b3f-0410-acfb-b9e37a522ad9>
2010-03-21 19:44:19 +0000

    fixed issue2 (again)

--- trunk/winzipaes/tests/src.test/de/idyl/crypto/zip/TestAesZipFileDecrypter.java
+++ trunk/winzipaes/tests/src.test/de/idyl/crypto/zip/TestAesZipFileDecrypter.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.idyl.crypto.zip.impl.ExtZipEntry;
@@ -21,15 +22,38 @@ public class TestAesZipFileDecrypter {
 		System.setProperty("java.util.logging.config.file", "logging.properties");
 	}
 
-	public final static String TEST_PATH = "tests/data/";
+	public final static String TEST_DATA_PATH = "tests/data/";
+
+	public final static String TEST_OUT_PATH = "tests/out/";
+
+	protected static boolean deleteDirectory(File path) {
+    if( path.exists() ) {
+      for( File file : path.listFiles() ) {
+         if(file.isDirectory()) {
+           deleteDirectory(file);
+         }
+         else {
+           file.delete();
+         }
+      }
+    }
+    return( path.delete() );
+	}
 	
+	@Before
+	public void setup() {
+		File outPath = new File( TEST_OUT_PATH );
+		deleteDirectory(outPath);
+		outPath.mkdirs();
+	}
+
 	@Test
 	public void test1FileInZipFile() throws Exception {
 		
 		activateLog();
 		
 		String password = "123456";
-		File aesFile = new File(TEST_PATH + "1winzipEncryptedFile.zip");
+		File aesFile = new File(TEST_DATA_PATH + "1winzipEncryptedFile.zip");
 
 		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
 		
@@ -39,7 +63,7 @@ public class TestAesZipFileDecrypter {
 	@Test
 	public void test2FilesInZipFile() throws Exception {
 		String password = "123456";
-		File aesFile = new File(TEST_PATH + "2winzipEncryptedFiles.zip");
+		File aesFile = new File(TEST_DATA_PATH + "2winzipEncryptedFiles.zip");
 
 		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
 		
@@ -49,7 +73,7 @@ public class TestAesZipFileDecrypter {
 
 	@Test
 	public void testModDateTime() throws ZipException, IOException {
-		File aesFile = new File(TEST_PATH + "1winzipEncryptedFile.zip");
+		File aesFile = new File(TEST_DATA_PATH + "1winzipEncryptedFile.zip");
 		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
 		List<ExtZipEntry> list = aesDecryptor.getEntryList();
 		
@@ -61,11 +85,11 @@ public class TestAesZipFileDecrypter {
 	public void test7zip() throws Exception {
 		String password = "123456789";
 		
-		File aesFile = new File(TEST_PATH + "7.zip");
+		File aesFile = new File(TEST_DATA_PATH + "7.zip");
 		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
 		List<ExtZipEntry> list = aesDecryptor.getEntryList();
 		
-		aesDecryptor.extractEntry( list.get(0), new File(TEST_PATH + "test7.txt"), password );
+		aesDecryptor.extractEntry( list.get(0), new File(TEST_OUT_PATH + "test7.txt"), password );
 	}
 
 	public static void activateLog() {
@@ -78,14 +102,53 @@ public class TestAesZipFileDecrypter {
 	
 	@Test
 	public void testDnz() throws Exception {		
-		
-		activateLog();
-		
+		//activateLog();
 		String password = "x123456789x";
-		File aesFile = new File(TEST_PATH + "testDNZ.zip");
+		File aesFile = new File(TEST_DATA_PATH + "testDNZ.zip");
 		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
 		List<ExtZipEntry> list = aesDecryptor.getEntryList();
-		aesDecryptor.extractEntry( list.get(0), new File(TEST_PATH + "testDNZ.txt"), password );
+		aesDecryptor.extractEntry( list.get(0), new File(TEST_OUT_PATH + "testDNZ.txt"), password );
+	}
+	
+	@Test
+	public void testWithUnEncryptedEntries() throws Exception {		
+		//activateLog();
+		String password = "foobar";
+		File aesFile = new File(TEST_DATA_PATH + "mixed.zip");
+		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
+		List<ExtZipEntry> list = aesDecryptor.getEntryList();		
+		aesDecryptor.extractEntry( list.get(0), new File(TEST_OUT_PATH + "bar"), password );
+	}
+
+	@Test(expected=ZipException.class)
+	public void testWithUnEncryptedEntriesExExpected() throws Exception, ZipException {		
+		//activateLog();
+		String password = "foobar";
+		File aesFile = new File(TEST_DATA_PATH + "mixed.zip");
+		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
+		List<ExtZipEntry> list = aesDecryptor.getEntryList();		
+		aesDecryptor.extractEntry( list.get(1), new File(TEST_OUT_PATH + "foo"), password );
+	}
+	
+	@Test
+	public void testWithSubdirs() throws Exception {		
+		activateLog();
+		String password = "foobar";
+		File aesFile = new File(TEST_DATA_PATH + "subdir.zip");
+		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
+		List<ExtZipEntry> list = aesDecryptor.getEntryList();
+		aesDecryptor.extractEntry( list.get(1), new File(TEST_OUT_PATH + "bar"), password );
+	}
+	
+	@Test
+	public void testIssue2() throws Exception {
+		String password = "test1234";
+		File aesFile = new File(TEST_DATA_PATH + "issue2.zip");
+		AesZipFileDecrypter aesDecryptor = new AesZipFileDecrypter(aesFile);
+		List<ExtZipEntry> list = aesDecryptor.getEntryList();		
+		//aesDecryptor.extractEntry( list.get(0), new File(TEST_OUT_PATH + "file1.txt"), password );
+		// entry 1 is a directory
+		aesDecryptor.extractEntry( list.get(2), new File(TEST_OUT_PATH + "dir1/file2.txt"), password );		
 	}
 	
 }
